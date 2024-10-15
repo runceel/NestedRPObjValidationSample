da3adff87cae3907930ba9d6a48eedbe65fd8269
Kazuki Ota <kaota@microsoft.com>
2024-10-15 16:48:18 +0900

    プロジェクト ファイルを追加します。

---
+++ NestedRPObjValidationSample/Models/SomeModel.cs
@@ -0,0 +1,23 @@
+﻿using Reactive.Bindings;
+using System.ComponentModel.DataAnnotations;
+
+namespace NestedRPObjValidationSample.Models;
+
+class SomeModel
+{
+    public ICollection<SomeEntity> SomeValues { get; } = [];
+}
+
+class SomeEntity
+{
+    [Required(ErrorMessage = "A is required.")]
+    public ValidatableReactiveProperty<string> A { get; set; }
+    [Required(ErrorMessage = "B is required.")]
+    public ValidatableReactiveProperty<string> B { get; set; }
+
+    public SomeEntity()
+    {
+        A = ValidatableReactiveProperty.CreateFromDataAnnotations("", () => A);
+        B = ValidatableReactiveProperty.CreateFromDataAnnotations("", () => B);
+    }
+}
---
+++ NestedRPObjValidationSample/Verify.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace NestedRPObjValidationSample;
+
+public static class Verify
+{
+    public static void ThrowIfNull([NotNull] object? value) => _ = value ?? throw new ArgumentNullException(nameof(value));
+}
